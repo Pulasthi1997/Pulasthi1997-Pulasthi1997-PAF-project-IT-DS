8c88ee9c85cca5f4f2069a73a836d7df9be318fc
“kavishkasheran” <kavishkasheran1111@gmail.com>
2020-04-09 15:20:04 +0530

    done

--- src/com/DoctorService.java
+++ src/com/DoctorService.java
@@ -1,5 +1,79 @@
 package com;
+import model.Doctor;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+@Path("/Doctors")
+public class DoctorService
+{
+ Doctor d1 = new Doctor();
+   @GET
+   @Path("/")
+@Produces(MediaType.TEXT_HTML)
+ 
+   public String readItems()
+   {
+   return d1.readItems();
+   }
+   @POST
+   @Path("/")
+   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+   @Produces(MediaType.TEXT_PLAIN)
+   public String insertItem(@FormParam("D_Name") String D_Name,
+    @FormParam("D_Type") String D_Type,
+    @FormParam("D_Contact_Number") String D_Contact_Number,
+    @FormParam("D_Address") String D_Address,
+    @FormParam("D_Email") String D_Email,
+    @FormParam("D_NIC") String D_NIC
+       )
+   
+   
+   
+   {
+    String output = d1.insertItem(D_Name,D_Type ,D_Contact_Number , D_Address,D_Email,D_NIC);
+   return output;
+   }
+   @PUT
+   @Path("/")
+   @Consumes(MediaType.APPLICATION_JSON)
+   @Produces(MediaType.TEXT_PLAIN)
+   public String updateItem(String dData)
+   {
+   //Convert the input string to a JSON object
+    JsonObject d12 = new JsonParser().parse(dData).getAsJsonObject();
+   //Read the values from the JSON object
+    String Doctor_ID = d12.get("Doctor_ID").getAsString();
+    String D_Name = d12.get("D_Name").getAsString();
+    String D_Type = d12.get("D_Type").getAsString();
+    String D_Contact_Number = d12.get("D_Contact_Number").getAsString();
+    String D_Address = d12.get("D_Address").getAsString();
+    String D_Email = d12.get("D_Email").getAsString();
+    String D_NIC = d12.get("D_NIC").getAsString();
+    
+   // String output = d1.updateItem(Doctor_ID, D_Name,D_Type , D_Contact_Number,D_Address,D_Email,D_NIC );
+   //return output;
+   String output = d1.updateItem(Doctor_ID, D_Name,D_Type , D_Contact_Number,D_Address,D_Email,D_NIC);
+   return output;
+   }
+   @DELETE
+   @Path("/")
+   @Consumes(MediaType.APPLICATION_XML)
+   @Produces(MediaType.TEXT_PLAIN)
+   public String deleteItem(String dData)
+   {
+   //Convert the input string to an XML document
+    Document doc = Jsoup.parse(dData, "", Parser.xmlParser());
 
-public class DoctorService {
+   //Read the value from the element <itemID>
+    String Doctor_ID = doc.select("Doctor_ID").text();
+    String output = d1.deleteItem(Doctor_ID);
+   return output;
+   }
 
 }
