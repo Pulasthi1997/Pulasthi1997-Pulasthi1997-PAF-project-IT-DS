8d850c0a397061508cf6ab7224ba46028604a222
Jaypraveen <praveen.fernando1998@gmail.com>
2020-04-10 00:02:17 +0530

    v2.14

--- src/model/Hospital.java
+++ src/model/Hospital.java
@@ -16,7 +16,7 @@ public class Hospital {
 		 Class.forName("com.mysql.jdbc.Driver");
 
 		 //Provide the correct details: DBServer/DBName, username, password
-		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_project", "root", "");
+		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_project?useTimezone=true&serverTimezone=UTC", "root", "");
 		 }
 		 catch (Exception e)
 		 {e.printStackTrace();}
