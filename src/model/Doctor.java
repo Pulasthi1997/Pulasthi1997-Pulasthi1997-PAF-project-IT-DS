878cfa5ace2540d01e08b171bdf96c9995cc72c1
Pulasthi1997 <pulasthikarunaratne@gmail.com>
2020-04-09 20:15:56 +0530

    server name changed in doctor

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -10,7 +10,7 @@ private Connection connect()
  Class.forName("com.mysql.jdbc.Driver");
 
  //Provide the correct details: DBServer/DBName, username, password
- con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/healthmanagement", "root", "");
+ con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/PAF_Project", "root", "");
  }
  catch (Exception e)
  {e.printStackTrace();}
