ea346a23e543da0af2a75d62dae5b7e4426d156a
“kavishkasheran” <kavishkasheran1111@gmail.com>
2020-04-09 14:16:04 +0530

    kasun

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -51,4 +51,56 @@ System.err.println(e.getMessage());
 }
 return output;
 } 
+public String readItems()
+{
+String output = "";
+try
+{
+Connection con = connect();
+if (con == null)
+{return "Error while connecting to the database for reading."; }
+// Prepare the html table to be displayed
+output = "<table border=\"1\"><tr><th>Doctor Name</th><th>Doctor specification</th><th>"
+		+ "Contact Number</th><th>Doctor Address</th><th>Doctor Email </th><th>Doctor NIC</th><th>Update</th><th>Remove</th></tr>";
+String query = "select * from doctor";
+Statement stmt = con.createStatement();
+ResultSet rs = stmt.executeQuery(query);
+// iterate through the rows in the result set
+while (rs.next())
+{
+String Doctor_ID = Integer.toString(rs.getInt("Doctor_ID"));
+String D_Name = rs.getString("D_Name");
+String D_Type = rs.getString("D_Type");
+//String itemPrice = Double.toString(rs.getDouble("itemPrice"));
+String D_Contact_Number = Integer.toString(rs.getInt("D_Contact_Number"));
+String D_Address = rs.getString("D_Address");
+String D_Email = rs.getString("D_Email");
+String D_NIC = rs.getString("D_NIC");
+
+
+// Add into the html table
+output += "<tr><td>" + D_Name + "</td>";
+output += "<td>" + D_Type + "</td>";
+output += "<td>" + D_Contact_Number + "</td>";
+output += "<td>" + D_Address + "</td>";
+output += "<td>" + D_Email + "</td>";
+output += "<td>" + D_NIC + "</td>";
+// buttons
+output += "<td><input name=\"btnUpdate\" type=\"button\"value=\"Update\" class=\"btn btn-secondary\"></td>"
+          + "<td><form method=\"post\" action=\"items.jsp\">"
+          + "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\"class=\"btn btn-danger\">"
++ "<input name=\"itemID\" type=\"hidden\" value=\"" + Doctor_ID
++ "\">" + "</form></td></tr>";
+}
+con.close(); 
+//Complete the html table
+output += "</table>";
+}
+catch (Exception e)
+{
+output = "Error while reading the items.";
+System.err.println(e.getMessage());
+}
+return output;
+} 
 }
