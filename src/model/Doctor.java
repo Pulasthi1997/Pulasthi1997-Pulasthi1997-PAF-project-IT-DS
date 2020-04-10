36413558cbf12f2463d7a5f43edbbf5e42f66c6e
“kavishkasheran” <kavishkasheran1111@gmail.com>
2020-04-10 21:24:36 +0530

    v2.33

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -77,9 +77,9 @@ public class Doctor { // A common method to connect to the DB
 				String D_Email = rs.getString("D_Email");
 				String D_NIC = rs.getString("D_NIC");
 
-// Add into the html table
-
-				output += "<tr><td>" + D_Name + "</td>";
+				//Add into the html table
+				output += "<tr><td><input id=\"hidDoctorIDUpdate\"name=\"hidDoctorIDUpdate\"type=\"hidden\" value=\"" + Doctor_ID + "\">"
+                                               + D_Name + "</td>";
 				output += "<td>" + D_Type + "</td>";
 				output += "<td>" + D_Contact_Number + "</td>";
 				output += "<td>" + D_Address + "</td>";
@@ -87,9 +87,9 @@ public class Doctor { // A common method to connect to the DB
 				output += "<td>" + D_NIC + "</td>";
 // buttons
 				output += "<td><input name=\"btnUpdate\" type=\"button\"value=\"Update\" class=\"btn btn-secondary\"></td>"
-						+ "<td><form method=\"post\" action=\"items.jsp\">"
+						+ "<td><form method=\"post\" action=\"DoctorInsert.jsp\">"
 						+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\"class=\"btn btn-danger\">"
-						+ "<input name=\"itemID\" type=\"hidden\" value=\"" + Doctor_ID + "\">" + "</form></td></tr>";
+						+ "<input name=\"hidDoctorIDDelete\" type=\"hidden\" value=\"" + Doctor_ID + "\">" + "</form></td></tr>";
 			}
 			con.close();
 //Complete the html table
